72f2048dc094ce06eed1db2e1fb9eab142bb9789
James N. V. Cash <james.nvc@gmail.com>
2012-02-03 09:33:20 -0500

    Initial Commit

---
+++ MusashiClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MusashiClient
+//
+//  Created by James Cash on 03-02-12.
+//  Copyright (c) 2012 University of Toronto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MusashiClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MusashiClient
+//
+//  Created by James Cash on 03-02-12.
+//  Copyright (c) 2012 University of Toronto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
